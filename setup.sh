471965fdf17cccaae38d7a2ade0eb79787310165
Aerows <hallin.daniel@gmail.com>
2018-04-19 08:15:17 +0200

    Added submodules properly

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/env.sh ~/.env.sh
-ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/dotfiles/lib/oh-my-zsh ~/.oh-my-zsh
