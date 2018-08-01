29584aa7acab939fa9a00e5837809e417e9a9805
daniel <daniel.hallin@visionists.se>
2018-08-01 13:58:31 +0200

    Made setupfile callable from everywhere and move .zshrc to secure place

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-stow zsh
-stow git
-stow tmux
-stow vim
+#!/usr/bin/env bash
+DOTDIR="$(dirname "$BASH_SOURCE")"
+
+mv $HOME/.zshrc $HOME/.zshrc.predotfiles
+stow -d $DOTDIR -t $HOME zsh
+stow -d $DOTDIR -t $HOME git
+stow -d $DOTDIR -t $HOME tmux
+stow -d $DOTDIR -t $HOME vim
